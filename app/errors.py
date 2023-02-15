26007c856e76ff4b93c8ec6c41608c6fbd3fad8b
= <=>
2023-02-16 02:28:29 +0300

    added new error message

--- app/errors.py
+++ app/errors.py
@@ -6,3 +6,5 @@ class Errors:
     def error_flag(self, _line: int, _word: str) -> str: print(f"You must use flag for create statement like name or description\n\tError at line: {_line} at word = {_word}")
     def error_process(self, _line: int, _word: str, _process: str) -> str: print(f"You must choose  process {_process}\n\tError: before from '{_word}' at line: {_line}");  
     def errror_templatenotFound(self, _line: int, _template: str) -> str: print(f"Template not found = {_template} at line: {_line}")
+    def error_filenotFound(self, _arg: int) -> str: print(f"Could not, File: {_arg}")
+    def error_alreadyFile(self, _arg: int) -> str: print(f"Already File with name: {_arg}")
